4f97e8ced4abafcadcb614c121bcfcfd3215bc6c
sewaustav <sewabobrov04@gmail.com>
2026-03-21 22:16:30 +0100

    redis + service

---
+++ CaseGo/internal/cache/contract.go
@@ -0,0 +1,14 @@
+package cache
+
+import (
+	"context"
+
+	"github.com/sewaustav/CaseGoCore/internal/cases/models"
+)
+
+type Interactor interface {
+	Push(ctx context.Context, inter *models.Interaction) error
+	GetFullHistory(ctx context.Context, dialogID int64) ([]models.Interaction, error)
+	DeleteLast(ctx context.Context, dialogID int64) error
+	Clear(ctx context.Context, dialogID int64) error
+}
