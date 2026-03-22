b7dd73ca546fbe64b95bdd30d8e3f18e43f3c409
sewaustav <sewabobrov04@gmail.com>
2026-03-22 19:47:49 +0100

    service

--- CaseGo/internal/cases/service/llm_service/llm.go
+++ CaseGo/internal/cases/service/llm_service/llm.go
@@ -8,9 +8,9 @@ import (
 )
 
 type LLM interface {
-	GenerateCase(ctx context.Context, topic, description string) (models.Case, error)
-	GenerateResponse(ctx context.Context, history []models.Interaction) (dto.CaseDto, error)
-	AnalyzeCase(ctx context.Context, conv []models.Interaction) (dto.Result, error)
+	GenerateCase(ctx context.Context, description string) (*models.Case, error)
+	GenerateResponse(ctx context.Context, history []models.Interaction) (*dto.CaseDto, error)
+	AnalyzeCase(ctx context.Context, conv []models.Interaction) (*dto.Result, error)
 }
 
 type LLMService struct {
