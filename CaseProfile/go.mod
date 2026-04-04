87f6b8419359a281544f4223c5667a97f743ae5f
sewaustav <sewabobrov04@gmail.com>
2026-04-04 03:50:21 +0200

    case profile repo

--- CaseProfile/go.mod
+++ CaseProfile/go.mod
@@ -3,6 +3,7 @@ module github.com/sewaustav/CaseGoProfile
 go 1.26.1
 
 require (
+	github.com/Masterminds/squirrel v1.5.4
 	github.com/gin-contrib/cors v1.7.7
 	github.com/gin-gonic/gin v1.12.0
 	github.com/golang-jwt/jwt/v5 v5.3.1
@@ -26,6 +27,8 @@ require (
 	github.com/goccy/go-yaml v1.19.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
--- CaseProfile/internal/case_profile/models/case_profile.go
+++ CaseProfile/internal/case_profile/models/case_profile.go
@@ -16,15 +16,16 @@ type UserIdentity struct {
 }
 
 type CaseProfile struct {
-	ID                   int64   `json:"id" db:"id"`
-	UserID               int64   `json:"user_id" db:"user_id"`
-	TotalCases           int64   `json:"total_cases" db:"total_cases"`
-	Assertiveness        float64 `json:"assertiveness" db:"assertiveness"`
-	Empathy              float64 `json:"empathy" db:"empathy"`
-	ClarityCommunication float64 `json:"clarity_communication" db:"clarity_communication"`
-	Resistance           float64 `json:"resistance" db:"resistance"`
-	Eloquence            float64 `json:"eloquence" db:"eloquence"`
-	Initiative           float64 `json:"initiative" db:"initiative"`
+	ID                   int64     `json:"id" db:"id"`
+	UserID               int64     `json:"user_id" db:"user_id"`
+	TotalCases           int64     `json:"total_cases" db:"total_cases"`
+	Assertiveness        float64   `json:"assertiveness" db:"assertiveness"`
+	Empathy              float64   `json:"empathy" db:"empathy"`
+	ClarityCommunication float64   `json:"clarity_communication" db:"clarity_communication"`
+	Resistance           float64   `json:"resistance" db:"resistance"`
+	Eloquence            float64   `json:"eloquence" db:"eloquence"`
+	Initiative           float64   `json:"initiative" db:"initiative"`
+	ChangedAt            time.Time `json:"changed_at" db:"changed_at"`
 }
 
 type CaseProfileHistory struct {
@@ -37,4 +38,20 @@ type CaseProfileHistory struct {
 	Eloquence            float64   `json:"eloquence" db:"eloquence"`
 	Initiative           float64   `json:"initiative" db:"initiative"`
 	Date                 time.Time `json:"actual_date" db:"actual_date"`
+}
+
+type CaseResult struct {
+	ID                   int64     `json:"id" db:"id"`
+	CaseID               int64     `json:"case_id" db:"case_id"`
+	UserID               int64     `json:"user_id" db:"user_id"`
+	DialogID             int64     `json:"dialog_id" db:"dialog_id"`
+	StepsCount           int32     `json:"steps_count" db:"steps_count"`
+	TokensUsed           int32     `json:"tokens_used" db:"tokens_used"`
+	FinishedAt           time.Time `json:"finished_at" db:"finished_at"`
+	Assertiveness        float32   `json:"assertiveness" db:"assertiveness"`
+	Empathy              float32   `json:"empathy" db:"empathy"`
+	ClarityCommunication float32   `json:"clarity_communication" db:"clarity_communication"`
+	Resistance           float32   `json:"resistance" db:"resistance"`
+	Eloquence            float32   `json:"eloquence" db:"eloquence"`
+	Initiative           float32   `json:"initiative" db:"initiative"`
 }
