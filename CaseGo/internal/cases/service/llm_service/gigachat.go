d09ca4eff7eadfedc10f3535d1e52082625503fc
sewaustav <sewabobrov04@gmail.com>
2026-04-22 23:45:53 +0200

    fix llm service

--- CaseGo/internal/cases/service/llm_service/gigachat.go
+++ CaseGo/internal/cases/service/llm_service/gigachat.go
@@ -206,9 +206,10 @@ func (g *GigaChatService) GenerateResponse(ctx context.Context, caseModel *model
 		{Role: "system", Content: systemPrompt},
 	}
 	for _, inter := range history {
-		messages = append(messages, gcMessage{Role: "user", Content: inter.Answer})
 		messages = append(messages, gcMessage{Role: "assistant", Content: inter.Question})
-		
+		if inter.Answer != "" {
+			messages = append(messages, gcMessage{Role: "user", Content: inter.Answer})
+		}
 	}
 
 	resp, err := g.chat(ctx, messages, 0.8)
