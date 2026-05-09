7b5b15a2cf425186d16bb0f77317850e54c23d79
sewaustav <sewabobrov04@gmail.com>
2026-05-09 23:26:08 +0200

    payments: feat - subscription as gift + bug fix

--- Payment/internal/payment/models/payment.go
+++ Payment/internal/payment/models/payment.go
@@ -16,11 +16,11 @@ type SubscriptionInfo struct {
 	ID               int64            `db:"id" json:"id"`
 	UserID           int64            `db:"user_id" json:"user_id"`
 	Subscription     SubscriptionType `db:"subscription" json:"subscription"`
-	FirstPaymentDate time.Time        `db:"first_payment_date" json:"first_payment_date"`
+	FirstPaymentDate *time.Time        `db:"first_payment_date" json:"first_payment_date"`
 	CountOfRenewal   int              `db:"count_of_renewal" json:"count_of_renewal"`
 	IsAutoRenew      bool             `db:"is_auto_renew" json:"is_auto_renew"`
-	LastPaymentDate  time.Time        `db:"last_payment_date" json:"last_payment_date"`
-	CanseledAt       time.Time        `db:"canceled_at" json:"canceled_at"`
+	LastPaymentDate  *time.Time        `db:"last_payment_date" json:"last_payment_date"`
+	CanseledAt       *time.Time        `db:"canceled_at" json:"canceled_at"`
 	ExpiredAt        time.Time        `db:"expired_at" json:"expired_at"`
 }
 
